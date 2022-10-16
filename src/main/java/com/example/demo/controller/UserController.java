b1b81a1f30c43493f26993a7fea4db789fa2a527
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-16 08:24:36 -0700

    create method for postmapping implement

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
@@ -12,5 +9,9 @@ public class UserController {
     @GetMapping("/getUser")
     public String getUser(){
         return "test get mapping data";
+    }
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "test save mapping";
     }
 }
