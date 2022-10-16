9b960362745ff6567d037742e8b16cf4e1f7c57f
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-16 09:58:31 -0700

    inject userservice

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/getUser")
     public String getUser(){
         return "test get mapping data";
