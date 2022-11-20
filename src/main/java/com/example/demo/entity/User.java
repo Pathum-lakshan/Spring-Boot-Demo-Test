169e883427d5407002dac15045b84fb741783d6d
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-11-20 21:51:56 +0530

    start Stored Procedure start

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,16 +4,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@NamedStoredProcedureQuery()
 public class User {
     @Id
     private int id;
     private String name;
     private String address;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+@NamedStoredProcedureQuery(name = "secondProcedure", procedureName = "getTicketsByCatagory", parameters = {
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "tcatagory", type = String.class) }) })*/
